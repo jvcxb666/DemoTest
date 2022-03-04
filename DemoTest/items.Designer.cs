f4835b7c9cc905975ab872f19c9f5e774cf4ba3b
jvcxb666 <56973806+jvcxb666@users.noreply.github.com>
2022-03-04 14:01:38 +0300

    added search and reset filters

--- DemoTest/items.Designer.cs
+++ DemoTest/items.Designer.cs
@@ -30,36 +30,74 @@ namespace DemoTest
         private void InitializeComponent()
         {
             this.datagrid = new System.Windows.Forms.DataGridView();
+            this.seachBox = new System.Windows.Forms.TextBox();
+            this.search_submit = new System.Windows.Forms.Button();
+            this.reset_button = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.datagrid)).BeginInit();
             this.SuspendLayout();
             // 
             // datagrid
             // 
             this.datagrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.datagrid.Location = new System.Drawing.Point(12, 96);
+            this.datagrid.Location = new System.Drawing.Point(12, 63);
             this.datagrid.Name = "datagrid";
             this.datagrid.RowHeadersWidth = 62;
             this.datagrid.RowTemplate.Height = 28;
-            this.datagrid.Size = new System.Drawing.Size(954, 342);
+            this.datagrid.Size = new System.Drawing.Size(954, 375);
             this.datagrid.TabIndex = 0;
             this.datagrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DG_CellContentClick);
+            // 
+            // seachBox
+            // 
+            this.seachBox.Location = new System.Drawing.Point(12, 21);
+            this.seachBox.Name = "seachBox";
+            this.seachBox.Size = new System.Drawing.Size(300, 26);
+            this.seachBox.TabIndex = 1;
+            this.seachBox.Text = "search by company";
+            // 
+            // search_submit
+            // 
+            this.search_submit.Location = new System.Drawing.Point(318, 12);
+            this.search_submit.Name = "search_submit";
+            this.search_submit.Size = new System.Drawing.Size(131, 45);
+            this.search_submit.TabIndex = 2;
+            this.search_submit.Text = "search";
+            this.search_submit.UseVisualStyleBackColor = true;
+            this.search_submit.Click += new System.EventHandler(this.search_submit_Click);
+            // 
+            // reset_button
+            // 
+            this.reset_button.Location = new System.Drawing.Point(835, 12);
+            this.reset_button.Name = "reset_button";
+            this.reset_button.Size = new System.Drawing.Size(131, 45);
+            this.reset_button.TabIndex = 3;
+            this.reset_button.Text = "reset filters";
+            this.reset_button.UseVisualStyleBackColor = true;
+            this.reset_button.Click += new System.EventHandler(this.reset_button_Click);
             // 
             // items
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 450);
+            this.Controls.Add(this.reset_button);
+            this.Controls.Add(this.search_submit);
+            this.Controls.Add(this.seachBox);
             this.Controls.Add(this.datagrid);
             this.Name = "items";
             this.Text = "items";
             this.Load += new System.EventHandler(this.items_Load);
             ((System.ComponentModel.ISupportInitialize)(this.datagrid)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView datagrid;
+        private System.Windows.Forms.TextBox seachBox;
+        private System.Windows.Forms.Button search_submit;
+        private System.Windows.Forms.Button reset_button;
     }
 }
--- DemoTest/items.cs
+++ DemoTest/items.cs
@@ -35,5 +35,33 @@ namespace DemoTest
             adapter.Fill(table);
             datagrid.DataSource = table.DefaultView;
         }
+
+        private void search_submit_Click(object sender, EventArgs e)
+        {
+            dbInfo db = new dbInfo();
+            MySqlConnection mysqli = db.mysqli;
+            string search_param = seachBox.Text;
+            MySqlCommand select = new MySqlCommand("SELECT * FROM `table` WHERE `company` LIKE @p", mysqli);
+            select.Parameters.Add("@p", MySqlDbType.VarChar).Value = "%" + search_param + "%";
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            mysqli.Open();
+            adapter.SelectCommand = select;
+            adapter.Fill(table);
+            datagrid.DataSource = table.DefaultView;
+        }
+
+        private void reset_button_Click(object sender, EventArgs e)
+        {
+            dbInfo db = new dbInfo();
+            MySqlConnection mysqli = db.mysqli;
+            MySqlCommand select = new MySqlCommand("SELECT * FROM `table` WHERE 1", mysqli);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            DataTable table = new DataTable();
+            mysqli.Open();
+            adapter.SelectCommand = select;
+            adapter.Fill(table);
+            datagrid.DataSource = table.DefaultView;
+        }
     }
 }
